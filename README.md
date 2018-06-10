db74f61f5b8885bda0b567e98159ae6aa8ae3e02
Pawan Kumar <pawan221b@gmail.com>
2018-06-10 15:14:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# random_pk
+
+A new Flutter package that gives a container with random color.
+
+## Getting Started
+
+For help getting started with Flutter, view our online [documentation](https://flutter.io/).
+
+For help on editing package code, view the [documentation](https://flutter.io/developing-packages/).
---
+++ lib/random_pk.dart
@@ -0,0 +1,49 @@
+library random_pk;
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class RandomColor {
+  static final Random _random = new Random();
+
+  /// Returns a random color.
+  static Color next() {
+    return new Color(0xFF000000 + _random.nextInt(0x00FFFFFF));
+  }
+}
+
+/// A Container Widget that takes up a given [width] and [height] and paints itself with a
+/// random color.
+
+class RandomContainer extends StatefulWidget {
+  final double width;
+  final double height;
+  final Widget child;
+
+  RandomContainer({this.width, this.height, this.child});
+
+  @override
+  _RandomContainerState createState() => new _RandomContainerState();
+}
+
+class _RandomContainerState extends State<RandomContainer> {
+  Color randomColor;
+
+  @override
+  void initState() {
+    super.initState();
+
+    randomColor = RandomColor.next();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: widget.width,
+      height: widget.height,
+      color: randomColor,
+      child: widget.child,
+    );
+  }
+}
